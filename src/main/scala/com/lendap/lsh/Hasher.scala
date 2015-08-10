110d0c175e22a8723f3cd89ca8e4ab5232e2fe13
cepnyci <aaytekin@gmail.com>
2015-08-10 08:59:25 +0300

    initial commit

---
+++ src/main/scala/com/lendap/lsh/Hasher.scala
@@ -0,0 +1,42 @@
+package com.lendap.lsh
+
+import org.apache.spark.mllib.linalg.SparseVector
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+
+/**
+ * Simple hashing function implements random hyperplane based hash functions described in
+ * http://www.cs.princeton.edu/courses/archive/spring04/cos598B/bib/CharikarEstim.pdf
+ * r is a random vector. Hash function h_r(u) operates as follows:
+ * if r.u < 0 //dot product of two vectors
+ *    h_r(u) = 0
+ *  else
+ *    h_r(u) = 1
+ */
+class Hasher(r: Array[Double]) extends Serializable {
+
+  /** hash SparseVector v with random vector r */
+  def hash(u : SparseVector) : Int = {
+    val rVec: Array[Double] = u.indices.map(i => r(i))
+    val hashVal = (rVec zip u.values).map(_tuple => _tuple._1 * _tuple._2).sum
+    if (hashVal > 0) 1 else 0
+  }
+
+}
+
+object Hasher {
+
+  /** create a new instance providing size of the random vector Array [Double] */
+  def create (size: Int) = new Hasher(r(size))
+
+  /** create a random vector whose whose components are -1 and +1 */
+  def r(size: Int) : Array[Double] = {
+    val buf = new ArrayBuffer[Double]
+    val rnd = new Random()
+    for (i <- 0 until size)
+      buf += (if (rnd.nextGaussian() < 0) -1 else 1)
+    buf.toArray
+  }
+
+}
---
+++ src/main/scala/com/lendap/lsh/Main.scala
@@ -0,0 +1,38 @@
+package com.lendap.lsh
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import java.io._
+
+import org.apache.spark.mllib.linalg.SparseVector
+
+
+/**
+ * Created by maytekin on 05.08.2015.
+ */
+object Main {
+
+  def main(args: Array[String]) {
+    val dataFile = "data/amazon-books-1m.data"
+    val conf = new SparkConf().setAppName("LSH").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile(dataFile).cache()
+    /*val pairs = data.map(line => line.split('\t'))
+      .map(l => (l(0), 1))
+      .reduceByKey(_ + _)
+      .filter{case (k, v) => v >= 20}
+      .map(x=> (x._2,x._1))
+    pairs.collect.foreach(println)
+    println(pairs.count)*/
+
+    val m = 100 /** number of elements */
+    val h = Hasher.create(m)
+    val i = Array(0, 2, 3, 6)
+    val v = Array(1.0, -4.0, 6.0, -20.0)
+    print(h.hash(new SparseVector(4, i, v)))
+
+  }
+
+}
