b00f443fabe8c3ddffbaa64538b2ee940ca40385
Maruf Aytekin <aaytekin@gmail.com>
2015-08-18 08:40:15 +0300

    rndRDD changes

--- src/main/scala/com/lendap/lsh/Main.scala
+++ src/main/scala/com/lendap/lsh/Main.scala
@@ -26,33 +26,36 @@ object Main {
     val ratingsRDD = data
       .map(line => line.split("\t"))
       .map(elems => (Rating(elems(0).toInt, elems(1).toInt, elems(2).toDouble)))
+
     val users = ratingsRDD.map(ratings => ratings.user).distinct()
     val items = ratingsRDD.map(ratings => ratings.product).distinct()
     val ratings50 = ratingsRDD.map(a => (a.user, (a.product, a.rating))).groupByKey().filter(a=>a._2.size > 50)
     val mostRatedMovies = ratingsRDD.map(a => a.product).countByValue.toSeq
-    println(mostRatedMovies.sortBy(-_._2).take(50).toList)
+    val spData = ratingsRDD.map(a => (a.user, (a.product, a.rating))).groupByKey()
+    .map(a=>(a._1, Vectors.sparse(a._2.size, a._2.toSeq)))
+    println(mostRatedMovies.sortBy(- _._2).take(50).toList)
     //val rating  = ratingsRDD.filter(a => a.user == 4904 && a.product == 2054)
     println(users.count() + " users rated on " +
       items.count() + " movies and "  +
       ratings50.count() + " users have more than 50 ratings.")
     val numHashFunc = 8
-    val sampleUser = ratings50.take(1)
+    //val sampleUser = ratings50.take(1)
+    //val (indices, values) = sampleUser(0)._2.toSeq.sortBy(_._1).unzip
+    //println(indices.size)
+    //println(values.size)
 
-    val (indices, values) = sampleUser(0)._2.toSeq.sortBy(_._1).unzip
-    println(indices.size)
-    println(values.size)
-
-    val sampleUserVec = sampleUser.map(a=>(a._1, Vectors.sparse(a._2.size, indices.toArray, values.toArray)))
+    //val sampleUserVec = sampleUser.map(a=>(a._1, Vectors.sparse(a._2.size, indices.toArray, values.toArray)))
     //val m = 100 /** number of elements */
     val maxElem = items.max()
     val size = items.count()
-    val randRDD = RandomRDDs.normalRDD(sc, maxElem, 8)
-    val h = Hasher.create(randRDD)
+    //run locality sensitive hashing
+    val lsh = new  LSH(ratingsRDD, numHashFunc = 6, numBands = 5)
+    val model = lsh.run
 
-    //val i = Array(0, 2, 3, 6)
-    //val v = Array(1.0, -4.0, 6.0, -20.0)
-    val (user, vec) = sampleUserVec(0)
-    print(h.hash(vec.asInstanceOf[SparseVector]))
+    val h = Hasher.create(maxElem)
+
+    //val (user, vec) = sampleUserVec(0)
+    //print(h.hash(vec.asInstanceOf[SparseVector]))
     println("")
   }
 
