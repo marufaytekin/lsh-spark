89c36d37d4f41baa4c83db3962970922536b2a7a
cepnyci <aaytekin@gmail.com>
2015-08-19 17:42:35 +0300

    initial commit

--- src/main/scala/com/lendap/lsh/LSHModel.scala
+++ src/main/scala/com/lendap/lsh/LSHModel.scala
@@ -12,32 +12,17 @@ import org.apache.spark.SparkContext._
 /** m: max number of elements in a vector */
 class LSHModel(m: Int, numHashFunc : Int, numBands: Int) extends Serializable {
 
-  /** generate numHashFunc hash functions */
+  /** generate numHashFunc * numBands hash functions */
   private val _hashFunctions = ListBuffer[Hasher]()
   for (i <- 0 until numHashFunc * numBands)
     _hashFunctions += Hasher.create(m)
   final val hashFunctions : List[(Hasher, Int)] = _hashFunctions.toList.zipWithIndex
 
-  /** the signature matrix with (hashFunctions.size signatures) */
-  var signatureMatrix : RDD[List[Int]] = null
-
-  /** the "bands" bandID, (hash key, vector_id) */
+  /** the "bands" bandID, (hash key, vector_id list) */
   var bands : RDD[(Int,(String, Iterable[Long]))] = null
 
-  /** (vector id, cluster id) */
-  var vector_cluster : RDD[(Long, Long)] = null
-
-  /** (cluster id, vector id) */
-  var cluster_vector : RDD[(Long, Long)] = null
-
-  /** (cluster id, List(Vector) */
-  var clusters : RDD[(Long, Iterable[SparseVector])] = null
-
-  /** filter out scores below threshold. this is an optional step.*/
-  //def filter(hashKey : String) : RDD = ???
-
-  //def compare(SparseVector v) : RDD
-
+  /** filter out buckets with hashKey.*/
+  def filter(hashKey : String) : RDD[(String, Iterable[Long])] = bands.filter(x => x._2._1 == hashKey).map(x => x._2)
 
 }
 
