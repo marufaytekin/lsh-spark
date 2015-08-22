ab8e0864e8dbaaa5b2224ffb27a273788c444da7
Maruf Aytekin <aaytekin@gmail.com>
2015-08-22 09:39:35 +0300

    prediction added.

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "lsh-scala"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.4.1"
+
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.10" % "1.4.1"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.1.3" % "test"
--- src/test/scala/com/lendap/lsh/LSHTestSuit.scala
+++ src/test/scala/com/lendap/lsh/LSHTestSuit.scala
@@ -1,44 +1,37 @@
 package com.lendap.lsh
 
-import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.mllib.linalg.{SparseVector, Vectors}
 import org.scalatest.FunSuite
 
 /**
  * Created by maruf on 09/08/15.
  */
-object LSHTestSuit extends FunSuite with LocalSparkContext {
+class LSHTestSuit extends FunSuite with LocalSparkContext {
 
-  test("simple test") {
+  test("hasher test") {
 
+    val data = List(
+      List(5.0,3.0,4.0,5.0,5.0,1.0,5.0,3.0,4.0,5.0).zipWithIndex.map(a=>a.swap),
+      List(1.0,2.0,1.0,5.0,1.0,5.0,1.0,4.0,1.0,3.0).zipWithIndex.map(a=>a.swap),
+      List(5.0,3.0,4.0,1.0,5.0,4.0,1.0,3.0,4.0,5.0).zipWithIndex.map(a=>a.swap),
+      List(1.0,3.0,4.0,5.0,5.0,1.0,1.0,3.0,4.0,5.0).zipWithIndex.map(a=>a.swap))
 
-    val data = List(List(21, 25, 80, 110, 143, 443),
-      List(21, 25, 80, 110, 143, 443, 8080),
-      List(80, 2121, 3306, 3389, 8080, 8443),
-      List(13, 17, 21, 23, 80, 137, 443, 3306, 3389))
-
-    val hasher = Hasher.create(10, 12345678)
-    val a1 = List(5.0,3.0,4.0,5.0,5.0,1.0,5.0,3.0,4.0,5.0).zipWithIndex.map(a=>a.swap)
-    val a2 = List(1.0,2.0,1.0,5.0,1.0,5.0,1.0,4.0,1.0,3.0).zipWithIndex.map(a=>a.swap)
-    val a3 = List(1.0,3.0,4.0,1.0,5.0,4.0,1.0,3.0,4.0,1.0).zipWithIndex.map(a=>a.swap)
-    val sv1 = Vectors.sparse(10, a1).asInstanceOf[SparseVector]
-    val sv2 = Vectors.sparse(10, a2).asInstanceOf[SparseVector]
-    val sv3 = Vectors.sparse(10, a3).asInstanceOf[SparseVector]
-    val hashKey = hasher.hash(sv1).toString + hasher.hash(sv2).toString + hasher.hash(sv3).toString
-
-
-
+    val h = Hasher.create(10, 12345678)
     val rdd = sc.parallelize(data)
 
     //make sure we have 4
     assert(rdd.count() === 4)
 
-    val vctr = rdd.map(r => (r.map(i => (i, 1.0)))).map(a => Vectors.sparse(65535, a).asInstanceOf[SparseVector])
+    //convert data to RDD of SparseVector
+    val vectorRDD = rdd.map(a => Vectors.sparse(a.size, a).asInstanceOf[SparseVector])
 
     //make sure we still have 4
-    assert(vctr.count() == 4)
+    assert(vectorRDD.count() === 4)
 
-    val h = Hasher.create(10, 123456)
+    val hashKey = vectorRDD.map(a => h.hash(a)).collect().mkString("")
+
+    //check if calculated hash key correct
+    assert(hashKey === "1010")
 
 
 
