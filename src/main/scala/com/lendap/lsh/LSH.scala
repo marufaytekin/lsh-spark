b5b6f018a1e11664c577ef322341dde786acf5bc
marufaytekin <aaytekin@gmail.com>
2015-08-27 15:20:42 +0300

    - updated readme. - license file added..

--- src/main/scala/com/lendap/lsh/LSH.scala
+++ src/main/scala/com/lendap/lsh/LSH.scala
@@ -4,42 +4,41 @@ package com.lendap.lsh
  * Created by maruf on 09/08/15.
  */
 
+import org.apache.spark.SparkContext
 import org.apache.spark.mllib.linalg.SparseVector
 import org.apache.spark.rdd.RDD
-import scala.collection.mutable.ListBuffer
-import org.apache.spark.SparkContext._
 
-class LSH(data : RDD[(Long, SparseVector)], size: Int, numHashFunc : Int, numBands : Int) extends Serializable {
+/** Build LSH model with data RDD. Hash each vector number of band times and stores in a bucket.
+  *
+  *
+  * @param data RDD of sparse vectors with vector Ids. RDD(vec_id, SparseVector)
+  * @param m max number of possible elements in a vector
+  * @param numHashFunc number of hash functions
+  * @param numBands number of bands. This parameter sometimes called hash tables as well.
+  *
+  * */
+class LSH(data : RDD[(Long, SparseVector)] = null, m: Int = 0, numHashFunc : Int = 4, numBands : Int = 4) extends Serializable {
 
-  /** Build LSH model. */
-  def model() : LSHModel = {
+  def run() : LSHModel = {
 
     //create a new model object
-    val model = new LSHModel(size, numHashFunc, numBands)
+    val model = new LSHModel(m, numHashFunc, numBands)
 
     val dataRDD = data.cache()
 
     //compute hash keys for each vector
     // - hash each vector numHashFunc times
     // - concat each hash value to create a hash key
-    // - position band id hash keys and associated vector ids into a new RDD.
-    val hashedDataRDD = dataRDD
-      .map(v => (model.hashFunctions.map(h => (h._1.hash(v._2), h._2 % numHashFunc)), v._1))
+    // - position band id hash keys and vector id into a new RDD.
+    // - creates RDD of ((band#, hash_key), vec_id) tuples.
+    model.bands = dataRDD
+      .map(v => (model.hashFunctions.map(h => (h._1.hash(v._2), h._2 % numBands)), v._1))
       .map(x => x._1.map(a => ((a._2, x._2), a._1)))
       .flatMap(a => a).groupByKey()
-      .map(x => ((x._1._1, x._2.mkString("")), x._1._2))
-      .groupByKey().cache()
-
-    //group items that hash together in the same bucket (band#, (hash_key, vec_id list))
-    model.bands = hashedDataRDD.map(x => (x._1._1, (x._1._2, x._2))).cache()
+      .map(x => ((x._1._1, x._2.mkString("")), x._1._2)).cache()
 
     model
-  }
 
-  /** hash a single vector against an existing model and return the candidate buckets */
-  def filter(data : SparseVector, model : LSHModel, itemID : Long) : RDD[Iterable[Long]] = {
-    val hashKey = model.hashFunctions.map(h => h._1.hash(data)).mkString("")
-    model.bands.filter(x => x._2._1 == hashKey).map(a => a._2._2)
   }
 
 }
