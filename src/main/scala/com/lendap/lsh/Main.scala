551fd16f23ce4a414658e6580d1134e2d6193662
Maruf Aytekin <aaytekin@gmail.com>
2015-09-07 22:01:40 +0300

    Cosine similarity usage added to readme.md.

--- src/main/scala/com/lendap/lsh/Main.scala
+++ src/main/scala/com/lendap/lsh/Main.scala
@@ -1,13 +1,10 @@
 package com.lendap.lsh
 
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 import org.apache.spark.mllib.linalg.{Vectors, SparseVector}
-import org.apache.spark.mllib.random.RandomRDDs
-import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.rdd.PairRDDFunctions
 
 
 /**
@@ -17,47 +14,75 @@ object Main {
 
   /** Sample usage of LSH on movie rating data.*/
   def main(args: Array[String]) {
-    //case class Rating[@specialized(Int, Long) ID](user: ID, item: ID, rating: Float)
+
+    //init spark context
     val numPartitions = 8
     val dataFile = "data/ml-1m.data"
     val conf = new SparkConf()
       .setAppName("LSH")
       .setMaster("local[4]")
     val sc = new SparkContext(conf)
+
     //read data file in as a RDD, partition RDD across <partitions> cores
-    val data = sc.textFile(dataFile)
+    val data = sc.textFile(dataFile, numPartitions)
+
+    //parse data and create (user, item, rating) tuples
     val ratingsRDD = data
       .map(line => line.split("::"))
-      .map(elems => Rating(elems(0).toInt, elems(1).toInt, elems(2).toDouble))
+      .map(elems => (elems(0).toInt, elems(1).toInt, elems(2).toDouble))
 
-    val users = ratingsRDD.map(ratings => ratings.user).distinct()
-    val items = ratingsRDD.map(ratings => ratings.product).distinct()
-    val maxElem = items.max + 1
-    println(maxElem)
-    val ratings50 = ratingsRDD.map(a => (a.user, (a.product, a.rating))).groupByKey().filter(a=>a._2.size > 50)
-    val mostRatedMovies = ratingsRDD.map(a => a.product).countByValue.toSeq
-    val userRatings = ratingsRDD.map(a => (a.user, (a.product, a.rating))).groupByKey()
-    val sampleRating = userRatings.take(1)(0)._2.toSeq
-    val spData = userRatings.map(a=>(a._1.toLong, Vectors.sparse(maxElem, a._2.toSeq).asInstanceOf[SparseVector]))
-    val sample = spData.take(1).toList
-    println(sample)
+    //list of distinct items
+    val items = ratingsRDD.map(x => x._2).distinct()
+    val maxIndex = items.max + 1
 
-    println(users.count() + " users rated on " +
-      items.count() + " movies and "  +
-      ratings50.count() + " users have more than 50 ratings.")
-    val numHashFunc = 8
+    //user ratings grouped by user_id
+    val userItemRatings = ratingsRDD.map(x => (x._1, (x._2, x._3))).groupByKey().cache()
 
-    val size = items.count()
-    //run locality sensitive hashing
-    val lsh = new  LSH(spData, maxElem, numHashFunc = 6, numBands = 4)
-    val model = lsh.run
+    //convert each user's rating to tuple of (user_id, SparseVector_of_ratings)
+    val sparseVectorData = userItemRatings
+      .map(a=>(a._1.toLong, Vectors.sparse(maxIndex, a._2.toSeq).asInstanceOf[SparseVector]))
 
-    model.filter(a => a._1._2 == "100100") foreach println
+    //run locality sensitive hashing model with 6 bands and 8 hash functions
+    val lsh = new LSH(sparseVectorData, maxIndex, numHashFunc = 8, numBands = 6)
+    val model = lsh.run()
 
+    //print sample hashed vectors in ((bandId#, hashValue), vectorId) format
+    model.bands.take(10) foreach println
 
-    model.save(sc,"target/output")
+    //get the near neighbors of userId: 4587 in the model
+    val candList = model.getCandidates(4587)
+    println("Number of Candidate Neighbors: ")
+    println(candList.count())
+    println("Candidate List: " + candList.collect().toList)
 
-    //model.filter("100010") foreach println*/
+    //save model
+    val temp = "target/" + System.currentTimeMillis().toString
+    model.save(sc, temp)
+
+    //load model
+    val modelLoaded = LSHModel.load(sc, temp)
+
+    //print out 10 entries from loaded model
+    modelLoaded.bands.take(15) foreach println
+
+    //create a user vector with ratings on movies
+    val movies = List(1,6,17,29,32,36,76,137,154,161,172,173,185,223,232,235,260,272,296,300,314,316,318,327,337,338,348)
+    val ratings = List(5.0,4.0,4.0,5.0,5.0,4.0,5.0,3.0,4.0,4.0,4.0,4.0,4.0,5.0,5.0,4.0,5.0,5.0,4.0,4.0,4.0,5.0,5.0,5.0,4.0,4.0,4.0)
+    val sampleVector = Vectors.sparse(maxIndex, movies zip ratings).asInstanceOf[SparseVector]
+    println(sampleVector)
+
+    //generate hash values for each bucket
+    val hashValues = model.hashValue(sampleVector)
+    println(hashValues)
+
+    //query LSH model for candidate set
+    val candidateList = model.getCandidates(sampleVector).collect()
+    println(candidateList.toList)
+
+    //compute similarity of sampleVector with users in candidate set
+    val candidateVectors = sparseVectorData.filter(x => candidateList.contains(x._1)).cache()
+    val similarities = candidateVectors.map(x => (x._1, lsh.cosine(x._2, sampleVector)))
+    similarities foreach println
 
   }
 
